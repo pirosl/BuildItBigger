531425e432c1adb996a6de0e010bce69d0a51b48
Lucian Piros <pirosl@yahoo.com>
2017-06-11 23:40:09 +0100

    refactor: button handler code moved into fragment

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,19 +1,22 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.androidjokedisplay.DisplayActivity;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements OnTaskCompleted {
 
     public MainActivityFragment() {
     }
@@ -31,6 +34,28 @@ public class MainActivityFragment extends Fragment {
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
                 .build();
         mAdView.loadAd(adRequest);
+
+        Button tellJokeBtn = (Button)root.findViewById(R.id.tell_joke_btn);
+
+        tellJokeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAsyncTask();
+            }
+        });
+
         return root;
+    }
+
+    void startAsyncTask() {
+        EndpointsAsyncTask endpointsAsyncTask = new EndpointsAsyncTask(this);
+        endpointsAsyncTask.execute();
+    }
+
+    @Override
+    public void onTaskCompleted(String result) {
+        Intent myIntent = new Intent(getActivity(), DisplayActivity.class);
+        myIntent.putExtra(DisplayActivity.JOKE_TEXT,result/*jokes.getJoke()*/);
+        startActivity(myIntent);
     }
 }
