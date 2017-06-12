303d7e43c1fe50699db21680381775c754d170d5
Lucian Piros <pirosl@yahoo.com>
2017-06-12 16:46:39 +0100

    fix: fix build environment

--- javajokeslib/src/main/java/com/udacity/gradle/javajokes/Jokes.java
+++ javajokeslib/src/main/java/com/udacity/gradle/javajokes/Jokes.java
@@ -1,7 +1,13 @@
 package com.udacity.gradle.javajokes;
 
+
+/**
+ * Somple jokes class provider
+ *
+ * @author Lucian Piros
+ */
 public class Jokes {
-    public String getJoke() {
-        return "This is a good joke";
+    public static String getJoke() {
+        return "This is a joke";
     }
 }
